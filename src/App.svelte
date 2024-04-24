85475b3b3260206bcac52cf7f144b75ca36095da
Lucas Rayan <53474519+Spishous@users.noreply.github.com>
2024-04-24 18:13:07 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Cowboy from './lib/Cowboy.svelte';
+</script>
+
+<main>
+  <h1>Cowboy</h1>
+    <div class="floor">
+      <div class="player">
+        <Cowboy />
+        <Cowboy reverse={true} />
+      </div>
+
+    </div>
+</main>
+
+<style>
+  .floor {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 45vh;
+    background-color: rgb(114, 78, 38);
+
+  }
+  .player {
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    font-size: 50px;
+    left: 2em;
+    right: 2em;
+    bottom: 100%;
+    margin-bottom: -60px;
+  }
+</style>
---
+++ src/lib/Cowboy.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let reverse = false
+
+    const imgCol = 7;
+    const imgRow = 6;
+
+    let imgFrame = 0;
+    let frameCoord = [0, 0];
+
+    setInterval(() => {
+        imgFrame++;
+        if (imgFrame +3 >= imgCol * imgRow) {
+            imgFrame = 0
+        }
+        frameCoord = [imgFrame % imgCol, Math.floor(imgFrame / imgCol)]
+    },300)
+</script>
+
+<div class="cowboy {reverse ? 'reverse' : ''}" style="background-position: -{frameCoord[0]}em -{frameCoord[1]}em" />
+
+<style>
+    .cowboy {
+        background-image: url('spriesheet-complete.png');
+        background-size: 7em 6em;
+        image-rendering: pixelated;
+        font-size: 120px;
+        width: 1em;
+        height: 1em;
+    }
+    .reverse {
+        transform: scaleX(-1);
+    }
+</style>
